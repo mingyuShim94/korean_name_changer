65d79e729359840f84e427b1afaa8f4b63a9961e
shimmingyu <alsrb60@petpuls.net>
2025-05-22 17:12:22 +0900

    loading 시간 단축

--- components/ui/fullscreen-loader.tsx
+++ components/ui/fullscreen-loader.tsx
@@ -9,7 +9,7 @@ interface FullScreenLoaderProps {
 
 export function FullScreenLoader({
   message = "Processing your request. This may take a little while...",
-  duration = 18,
+  duration = 10,
 }: FullScreenLoaderProps) {
   const [timeLeft, setTimeLeft] = React.useState(duration > 0 ? duration : 0);
   const [tipIndex, setTipIndex] = React.useState(0);
